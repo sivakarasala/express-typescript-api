536c7f6ffe59e5ff69c7e20410f55edf4e71bf98
Siva Krishna <shivataraya@gmail.com>
2019-12-28 15:39:07 -0600

    add routes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("aum namah shivaya");
+import express, { Request, Response } from "express";
+import { router } from "./routes/loginRoutes";
+
+const app = express();
+
+app.use(router);
+
+app.listen(3000, () => {
+  console.log("hara hara mahadeva");
+});
